865a7f6d477a2a35d2c3d76b512930d8ec6ddb39
gEvent <gevent@CSSI2015-15.local>
2015-07-30 11:02:56 -0400

    new event works now

--- main.py
+++ main.py
@@ -227,57 +227,77 @@ class FormHandler(webapp2.RequestHandler):
         user = users.get_current_user()
         template_data = {'user': user, 'logout_link': users.create_logout_url('/'), 'nickname': "DEFAULT" if not user else user.nickname(), 'login_link': users.create_login_url('/')}
         template = jinja_environment.get_template('templates/event.html')
-        if self.request.get('latitude') != "" and self.request.get('longitude') != "":
-            next_event = Event(name = self.request.get('name'),
-                                place = self.request.get('venue_name'),
-                                description= self.request.get('description'),
-                                address = self.request.get('venue_address'),
-                                city= self.request.get('city_name'),
-                                region = self.request.get('region_name'),
-                                zip_code= self.request.get('postal_code'),
-                                country = self.request.get('country_abbr'),
-                                place_url= self.request.get('venue_url'),
-                                start_time = self.request.get('start_time'),
-                                frequency= self.request.get('recur_string'),
-                                lat_lon = [float(self.request.get('latitude')), float(event['longitude'))]
-                                # pictures[0]= event['description'],
-                                )
-        else:
-            next_event = Event(name = self.request.get('name'),
-                                place = self.request.get('venue_name'),
-                                description= self.request.get('description'),
-                                address = self.request.get('venue_address'),
-                                city= self.request.get('city_name'),
-                                region = self.request.get('region_name'),
-                                zip_code= self.request.get('postal_code'),
-                                country = self.request.get('country_abbr'),
-                                place_url= self.request.get('venue_url'),
-                                start_time = self.request.get('start_time'),
-                                frequency= self.request.get('recur_string'),
-                                # pictures[0]= event['description'],
-                                )
+        # if self.request.get('latitude') != "" and self.request.get('longitude') != "":
+        #     next_event = Event(name = self.request.get('name'),
+        #                         place = self.request.get('venue_name'),
+        #                         description= self.request.get('description'),
+        #                         address = self.request.get('venue_address'),
+        #                         city= self.request.get('city_name'),
+        #                         region = self.request.get('region_name'),
+        #                         zip_code= self.request.get('postal_code'),
+        #                         country = self.request.get('country_abbr'),
+        #                         place_url= self.request.get('venue_url'),
+        #                         start_time = self.request.get('start_time'),
+        #                         frequency= self.request.get('recur_string'),
+        #                         lat_lon = [float(self.request.get('latitude')), float(event['longitude'))]
+        #                         # pictures[0]= event['description'],
+        #                         )
+        # else:
+        next_event = Event(name = self.request.get('name'),
+                            place = self.request.get('venue_name'),
+                            description= self.request.get('description'),
+                            address = self.request.get('venue_address'),
+                            city= self.request.get('city_name'),
+                            region = self.request.get('region_name'),
+                            zip_code= self.request.get('postal_code'),
+                            country = self.request.get('country_abbr'),
+                            place_url= self.request.get('venue_url'),
+                            start_time = self.request.get('start_time'),
+                            frequency= self.request.get('recur_string'),
+                            # pictures[0]= event['description'],
+                            )
+        # logging.info(self.request.get('name'))
+        # logging.info(self.request.get('venue_name'))
+        # logging.info(self.request.get('description'))
+        # logging.info(self.request.get('venue_address'))
+        # logging.info(self.request.get('city_name'))
+        # logging.info(self.request.get('region_name'))
+        # logging.info(self.request.get('postal_code'))
+        # logging.info(self.request.get('country_abbr'))
+        # logging.info(self.request.get('venue_url'))
+        # logging.info(self.request.get('start_time'))
+        # logging.info(self.request.get('recur_string'))
+
+        template_data['name'] = next_event.name
+        template_data['place'] = next_event.place
+        template_data['description'] = next_event.description
+        template_data['address'] = next_event.address
+        template_data['city'] = next_event.city
+        template_data['region'] = next_event.region
+        template_data['zip_code'] = next_event.zip_code
+        template_data['country'] = next_event.country
+        template_data['start_time'] = next_event.start_time
+        template_data['frequency'] = next_event.frequency
+
         match = False
         name = next_event.name
-        next_event = next_event.put()
-        # event_id = str(next_event.key.id())
-        # template_data['id']= str(next_event.key.id())
-        logging.info("======== lets see the ID")
-        logging.info(event_id)
-        next_event = next_event.get()
+        next_event = next_event.put().get()
+        event_id = str(next_event.key.id())
+        # template_data['id']= str(next_event.id())()
         start_time = next_event.start_time
         all_events = Event.query()
-        for each_event in all_events:
-            if name == each_event.name and start_time == each_event.start_time:
-                match = True
-                logging.info('this event exists already')
-        if not match:
-            next_event = next_event.put()
-            next_event = next_event.get()
-        result = "<a href='/event?id=" + event_id + "'>" + event['title'] + " at " + event['venue_name'] + "</a><br>" # "<a href='/event?id=%s> %s at %s</a><br>" % (event_id, event['title'], event['venue_name'])
+        # for each_event in all_events:
+        #     if name == each_event.name and start_time == each_event.start_time:
+        #         match = True
+        #         logging.info('this event exists already')
+        # if not match:
+        #     next_event = next_event.put()
+        #     next_event = next_event.get()
+        result = "<a href='/event?id=" + event_id + "'>" + next_event.name + " at " + next_event.place + "</a><br>" # "<a href='/event?id=%s> %s at %s</a><br>" % (event_id, event['title'], event['venue_name'])
 
 
         template_data['results'] = result
-        self.response.write(result_template.render(template_data))
+        self.response.write(template.render(template_data))
 
 
 class MapHandler(webapp2.RequestHandler):
